beef0edf45590e8296006441331700da66199d98
Lee EunSoo <88019375+ieunsoo@users.noreply.github.com>
2025-07-25 17:53:17 +0900

    feature 툴바 & 사이드 바 연결 why. canvasView 안에서 사용할 툴바와 사이드바를 구현하기 위해서 how. SideBarToggleView안에 toolbar와 sidebar연결

--- Projects/Rootrip/Rootrip/Views/Componants/MapCanvasToolBar.swift
+++ Projects/Rootrip/Rootrip/Views/Componants/MapCanvasToolBar.swift
@@ -2,32 +2,62 @@ import SwiftUI
 
 //TODO: 툴바도 두개로 분리해 주어야 함
 struct MapCanvasToolBar: View {
-    @Binding var showMapCanvas: Bool
+//    @Binding var showMapCanvas: Bool
+    @State var isCanvasLocked: Bool = false
+    @Binding var isSidebarOpen: Bool
     
     var body: some View {
         VStack(spacing: 0){
-            ZStack {
-                HStack{
-                    Image(systemName: "sidebar.left")
-                        .padding(.leading, 20)
+            ZStack (alignment: .topLeading){
+                HStack(spacing: 20){
+                    Button(action:{
+                        withAnimation(){
+                            isSidebarOpen.toggle()
+                        }
+                    }){
+                        Image(systemName: "sidebar.left")
+                            .padding(.leading, 20)
+                    }
                     Spacer()
-                    Text("TPAP 우정여행")
+                    Button(action: {
+                        
+                    }){
+                        Text("TPAP 우정여행")
+                        Image(systemName: "chevron.down")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 10)
+                    }
                     Spacer()
                     Image(systemName: "arrow.uturn.backward")
                     Image(systemName: "arrow.uturn.forward")
                     
-                    Text("잠금")
-                    Text("저장")
-                    Button(action: {
+                    Button(action:{
                         withAnimation(.linear){
-                            showMapCanvas = false
+                            isCanvasLocked.toggle()
                         }
+                    }){
+                        Image(systemName: isCanvasLocked ? "lock.fill" : "lock.open.fill")
+                        .frame(width: 20)
+                    }
+                    
+                    Button(action:{
+                        //저장 로직 여기에 입력
+                    }){
+                        Text("저장")
+                    }
+                    
+                    Button(action: {
+//                        withAnimation(.linear){
+//                            showMapCanvas = false
+//                        }
                     }){
                         Text("홈")
                     }
-                        .padding(.trailing, 20)
+                        .padding(.trailing, 30)
                 }
                 .foregroundStyle(.white)
+                
             }
             .frame(height: 50) // 툴바 콘텐츠의 높이 지정
             .background(
@@ -35,28 +65,13 @@ struct MapCanvasToolBar: View {
                     .ignoresSafeArea(.container, edges: .top)
             )
             
-            ZStack {
-                HStack{
-                    Spacer()
-                    //커스텀 슬라이더 여기로 와야함
-                    Slider(value: .constant(0.5), in: 0...1)
-                        .frame(width: 100)
-                    
-                    Image(systemName: "eraser")
-                    Image(systemName: "pencil.circle.fill")
-                    Image(systemName: "pencil.tip.crop.circle.fill")
-                    Image(systemName: "paintpalette")
-                        .padding(.trailing, 50)
-                }
-            }
-            .frame(height: 50) // 툴바 콘텐츠의 높이 지정
-            .background(
-                Color.white
-            )
+            
         }
     }
 }
 
 //#Preview {
 //    MapCanvasToolBar()
+//        .environmentObject(PlanManager())
+//        .environmentObject(RouteManager())
 //}
