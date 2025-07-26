734470519334be67540ed0df403f47760d9e2188
송헌_Dean <147568611+whatdoIsa@users.noreply.github.com>
2025-07-26 17:36:14 +0900

    #112 맵 구현Test

--- Projects/Rootrip/Rootrip/Views/Map/POIAnnotation.swift
+++ Projects/Rootrip/Rootrip/Views/Map/POIAnnotation.swift
@@ -1,5 +1,8 @@
 import MapKit
 
+// MARK: - POIAnnotation
+/// POIAnnotation 클래스는 MKMapItem 정보를 기반으로
+/// 지도에 표시할 수 있는 어노테이션 객체를 생성
 class POIAnnotation: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
     let title: String?
