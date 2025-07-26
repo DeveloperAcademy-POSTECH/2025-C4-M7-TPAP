7ca73b83f5f4b4eee6a5334b49aaf730f8892da3
송헌_Dean <147568611+whatdoIsa@users.noreply.github.com>
2025-07-26 17:36:14 +0900

    #112 맵 구현Test

--- Projects/Rootrip/Rootrip/Models/RepositoryProtocols/POISearchRepositoryProtocol.swift
+++ Projects/Rootrip/Rootrip/Models/RepositoryProtocols/POISearchRepositoryProtocol.swift
@@ -1,7 +1,16 @@
 import MapKit
 
-
+// MARK: - POISearchRepositoryProtocol
+/// POI 검색 Repository의 프로토콜을 정의합니다.
+/// 구현체는 이 프로토콜을 준수하여 키워드 및 지도 영역 기반으로 POI 검색 기능을 제공
 protocol POISearchRepositoryProtocol {
-    func searchPOIs(for query: String, region: MKCoordinateRegion, completion: @escaping ([MKMapItem]) -> Void)
+    /// 키워드와 지도 영역을 기반으로 POI 검색을 실행하는 메서드
+    /// - Parameters:
+    ///   - query: 검색할 키워드 문자열
+    ///   - region: 검색을 제한할 지도 영역 (MKCoordinateRegion)
+    ///   - completion: 검색 결과인 MKMapItem 배열을 반환하는 클로저
+    func searchPOIs(for query: String,
+                    region: MKCoordinateRegion,
+                    completion: @escaping ([MKMapItem]) -> Void)
 }
 
