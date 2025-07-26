f246ce8d4156b7993937c3903dbeb21ac5b2086b
Lee EunSoo <88019375+ieunsoo@users.noreply.github.com>
2025-07-27 01:31:14 +0900

    feature41 툴바 사이드바 연결 why map 캔버스에서 툴바가 의도했던 디자인으로 보이도록 구현하기 위함 how. SidebarToggleView에 만들었던 sidebar, toolbar, toolpicker연결

---
+++ Projects/Rootrip/Rootrip/Views/Componants/MapCanvasToolPicker.swift
@@ -0,0 +1,49 @@
+//
+//  MapCanvasToolPicker.swift
+//  Rootrip
+//
+//  Created by eunsoo on 7/25/25.
+//
+
+import SwiftUI
+
+struct MapCanvasToolPicker: View {
+    var body: some View {
+        ZStack {
+            HStack(spacing: 10){
+                Spacer()
+                
+                PenThicknessSlider()
+                    .padding(.trailing, 20)
+                
+                Button(action: {}) {
+                    Image(systemName: "eraser")
+                }
+                //map위에 호버팅 버튼으로 빠질 예정이라 주석처리
+//                Button(action: {}) {
+//                    Image(systemName: "pencil.circle.fill")
+//                }
+                Button(action: {}) {
+                    Image(systemName: "pencil.tip.crop.circle.fill")
+                }
+                Button(action: {}) {
+                    Image(systemName: "paintpalette")
+                }
+                    .padding(.trailing, 50)
+            }
+            .foregroundStyle(Color.black)
+            
+        }
+        .frame(height: 50) // 툴바 콘텐츠의 높이 지정
+        .background(
+            Color.mainbackground
+        )
+        
+        
+    }
+}
+
+
+#Preview {
+    MapCanvasToolPicker()
+}
