24a8cf6a53a92e17edd794047c4f82fd4a45d504
송헌_Dean <147568611+whatdoIsa@users.noreply.github.com>
2025-07-28 14:54:52 +0900

    Update ProjectCard

--- Projects/Rootrip/Rootrip/Views/Componants/ProjectCard.swift
+++ Projects/Rootrip/Rootrip/Views/Componants/ProjectCard.swift
@@ -8,62 +8,7 @@ struct ProjectCard: View {
     
     var body: some View {
         ZStack(alignment: .topLeading) {
-            // TODO: 지도 이미지 들어가도록 만들어야함
-            Rectangle()
-                .foregroundStyle(.white)
-                .frame(maxWidth: isHighlighted ? 1070 : 325, maxHeight: isHighlighted ? 380 : 190)
-                .clipped()
-                .overlay(
-                    ZStack {
-                        LinearGradient(
-                            gradient: Gradient(colors: [Color.primary1.opacity(0.7), .clear]),
-                            startPoint: .bottom,
-                            endPoint: .center
-                        )
-                        
-                        //카드에 들어가는 텍스트 (제목, 날짜 등)
-                        VStack {
-                            Spacer()
-                            HStack {
-                                // 여행 제목
-                                Text(project.title)
-                                    .font(.system(size: isHighlighted ? 32 : 18, weight: .bold))
-                                    .foregroundColor(.white)
-                                    .multilineTextAlignment(.leading)
-                                
-                                Spacer()
-                                
-                                // 날짜 표시
-                                VStack(alignment: .trailing, spacing: 4) {
-                                    if let end = project.endDate {
-                                        VStack (spacing: 4) {
-                                            Text(formattedDate(project.startDate))
-                                            
-                                            Rectangle()
-                                                .fill(Color.white)
-                                                .frame(width:isHighlighted ? 20: 14,
-                                                       height: 1)
-                                            
-                                            Text(formattedDate(end))
-                                        }
-                                    } else {
-                                        Text(formattedDate(project.startDate))
-                                    }
-                                }
-                                .font(.system(size: isHighlighted ? 20 : 12, weight: .bold))
-                                .foregroundColor(.white.opacity(0.9))
-                            }
-                            .padding(isHighlighted ? 24 : 16)
-                        }
-                    }
-                )
-                .cornerRadius(16)
-                .shadow(color: .gray.opacity(0.4),
-                        radius: isHighlighted ? 8 : 4, x: 0, y: 2)
-            
-            
-            // 편집 모드일 때 체크 동그라미
-            //TODO: asset정해지면 체크박스 색상 수정해야함
+            normalCardView
             if isEditing {
                 editCardView
             }
